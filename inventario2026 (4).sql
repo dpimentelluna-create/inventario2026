c4fae1602195cd682e54e947c86953ed51f97901
Snayder902 <gynekkoyt5907@gmail.com>
2026-09-02 18:03:36 +0300

    Update inventario2026 (4).sql

--- inventario2026 (4).sql
+++ inventario2026 (4).sql
@@ -1,3 +1,18 @@
+consulta SQL: Copiar
+
+
+TRUNCATE TABLE docentes;
+MySQL ha dicho: Documentación
+
+#1701 - Cannot truncate a table referenced in a foreign key constraint (`inventario2026`.`prestamos`, CONSTRAINT `prestamos_docente_id_foreign` FOREIGN KEY (`docente_id`) REFERENCES `inventario2026`.`docentes` (`id`))
+
+
+
+
+
+
+
+
 SET FOREIGN_KEY_CHECKS = 0;
 
 TRUNCATE TABLE docentes;
